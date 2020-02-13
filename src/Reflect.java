8641fdfa42029a6b8306e982ea0adb2221eee8ef
ShreyesBhat <32994823+ShreyesBhat@users.noreply.github.com>
2020-02-13 16:17:48 +0530

    Add files via upload

---
+++ src/Reflect.java
@@ -0,0 +1,36 @@
+import java.lang.annotation.Inherited;
+import java.lang.reflect.*;
+public class Reflect
+{
+    Class reflectClass = StudentInfo.class;
+    Reflect() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        System.out.println("Reflection Class created\n");
+        String className = reflectClass.getName();
+        Class[] ifaces = reflectClass.getInterfaces();
+        for(Class i: ifaces)
+        {
+            System.out.println("Implemented Interface " + i);
+        }
+        System.out.println("Class name: " + className);
+        Method[] classMethods = reflectClass.getDeclaredMethods();
+        for (Method method : classMethods) {
+            System.out.print("Method Name: " + method.getName() + "    ");
+            if (method.getName().startsWith("get")) {
+                System.out.println("Type: Getter Method");
+            } else if (method.getName().startsWith("set")) {
+                System.out.println("Type: Setter Method");
+            }
+        }
+        caller();
+    }
+
+    private void caller() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        StudentInfo obj = new StudentInfo();
+        Method setter = reflectClass.getDeclaredMethod("setName", String.class);
+        Method getter = reflectClass.getDeclaredMethod("getName");
+        System.out.format("Name is currently set as %s \n", (String) getter.invoke(obj));
+        setter.invoke(obj, "wxyz");
+        String name = (String) getter.invoke(obj);
+        System.out.format("setName invoked by reflection class. Changed name is %s", name);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,44 @@
+import java.lang.*;
+import java.lang.reflect.InvocationTargetException;
+
+class StudentInfo
+{
+    private String name;
+    private int usn;
+
+    public StudentInfo(String name, int usn)
+    {
+        this.name = name;
+        this.usn = usn;
+    }
+    public StudentInfo()
+    {
+        this.name = "Abcd";
+        this.usn = 98;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getUsn()
+    {
+        return  usn;
+    }
+    public void setUsn(int usn)
+    {
+        this.usn = usn;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+
+public class Student {
+    public static void main(String args[]) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        StudentInfo st = new StudentInfo("abcd",98);
+        System.out.println(st.getName());
+        Reflect ref = new Reflect();
+    }
+}
